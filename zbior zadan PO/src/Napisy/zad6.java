a055bcc30e95a76c9f8f2ae9fdc628747c6b0ff3
SzymonBog <szymonbog77@gmail.com>
2023-12-03 15:18:20 +0100

    8 1-6

--- zbior zadan PO/src/Napisy/zad6.java
+++ zbior zadan PO/src/Napisy/zad6.java
@@ -2,7 +2,7 @@ package Napisy;
 
 public class zad6 {
     public static void main(String[] args) {
-        String str = "Jakiś napis testujacy";
+        String str = "Jakiś napis testujacyy";
         for (int i = 0; i < str.length(); i++) {
             if (i % 2 == 0) {
                 System.out.println(str.charAt(i));
